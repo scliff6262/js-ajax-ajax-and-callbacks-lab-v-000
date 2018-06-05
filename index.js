f01aa222c97a1b2f558aa98d6b86360064b942af
Sean Clifford <sclifford6262@gmail.com>
2018-06-05 02:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 $(document).ready(function (){
-  function searchRepositories(){
-    const searchTerms = document.getElementById("#searchTerms")
-    $.get("https://api.github.com/search/repositories?q=" + searchTerms, function(data){
-      console.log(data)
-    })
-  }
 
 });
+
+function searchRepositories(){
+  const searchTerms = document.getElementById("#searchTerms")
+  $.get("https://api.github.com/search/repositories?q=" + searchTerms, function(data){
+    console.log(data)
+  })
+}
