bbe5a1a3bf80d56a4dc1e600287910ec06c0069e
Sean Clifford <sclifford6262@gmail.com>
2018-06-05 02:55:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ $(document).ready(function (){
 function searchRepositories(){
   const searchTerms = document.getElementById("searchTerms").value
   $.get("https://api.github.com/search/repositories?q=" + searchTerms, function(data){
-    console.log(JSON.parse(data))
-    /*const results = JSON.parse(data)
+    console.log(data)
+    const results = JSON.parse(data)
     const src = document.getElementById("repo-template").innerHTML
     const template = Handlebars.compile(src)
     const repoList = template(results)
-    document.getElementById("results").innerHTML = repoList */
+    document.getElementById("results").innerHTML = repoList
   })
 }
