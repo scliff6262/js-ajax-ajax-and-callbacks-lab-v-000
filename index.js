579a44cb50164098c7c2c734e51b81d493ced9db
Sean Clifford <sclifford6262@gmail.com>
2018-06-05 04:12:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ $(document).ready(function (){
 function searchRepositories(){
   const searchTerms = document.getElementById("searchTerms").value
   $.get("https://api.github.com/search/repositories?q=" + searchTerms).done(function(data){
-    /*const src = document.getElementById("repo-template").innerHTML
-    const template = Handlebars.compile(src)
-    const repoList = template(data.items)
-    dontBreak = data.items
-    document.getElementById("results").innerHTML = repoList */
     $("#results").html(data.items.map(function(obj){
       return `<li>
       <h3><a href="${obj.html_url}">${obj.name}</a></h3>
