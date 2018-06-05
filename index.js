2d52fb54a99f9096ebcdfdd0012a14a1220d2a6d
Sean Clifford <sclifford6262@gmail.com>
2018-06-05 04:08:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ function searchRepositories(){
     const repoList = template(data.items)
     dontBreak = data.items
     document.getElementById("results").innerHTML = repoList */
-    function item(obj){return `<li>
+    function item(obj){
+      return `<li>
       <h3><a href="${html_url}">${name}</a></h3>
       <p>${description}</p>
       <p><a href="${owner.html_url}">${owner.login}</a></p>
       <p><img src="${owner.avatar_url}" alt="User Avatar" height="50" width="50"></p>
     </li>`};
-    $("#results").html(data.items.map(tem))
+    $("#results").html(data.items.map(item.(obj)))
   })
 }
